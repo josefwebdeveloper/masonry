62b4bdc5ea352b555f65c9a6802039c85515c5a4
Josef Shainskiy <josef2011@gmail.com>
2020-12-03 16:02:39 +0200

    n

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { Test1Component } from './test1/test1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotfoundComponent,
+    Test1Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/test1/test1.component.ts
@@ -0,0 +1,44 @@
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-test1',
+  templateUrl: './test1.component.html',
+  styleUrls: ['./test1.component.scss']
+})
+export class Test1Component implements OnInit, AfterViewInit {
+
+  constructor() {
+  }
+
+  ngAfterViewInit() {
+    this.resizeAllGridItems();
+    window.addEventListener('resize', this.resizeAllGridItems.bind(this));
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  resizeGridItem = (item: any) => {
+    const grid = document.getElementsByClassName('grid')[0];
+    const rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
+    const rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
+    const rowSpan = Math.ceil((item.querySelector('.content').getBoundingClientRect().height + rowGap) / (rowHeight + rowGap));
+    item.style.gridRowEnd = 'span ' + rowSpan;
+  };
+
+  resizeAllGridItems(): void {
+    const allItems = document.getElementsByClassName('item');
+    for (let x = 0; x < allItems.length; x++) {
+      this.resizeGridItem(allItems[x]);
+    }
+  }
+
+  // resizeInstance(instance){
+  //    const item = instance.elements[0];
+  //    this.resizeGridItem(item);
+  //  }
+
+// window.onload = resizeAllGridItems();
+
+}
